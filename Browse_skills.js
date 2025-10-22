6fc35304ba7c24bd3b98312d0d2544d059b7f9b2
chiragkhosla <chiraguser1@gmail.com>
2025-10-22 10:34:06 +0530

    add feature to display saved skill cards on browse page

--- Browse_skills.js
+++ Browse_skills.js
@@ -29,3 +29,19 @@ function filter(categoryText){
 function filterByCategory(category) {
     filter(category);
 }
+window.onload = function() {
+            const container = document.getElementById("cardsContainer");
+            const cards = JSON.parse(localStorage.getItem("cards")) || [];
+
+            for (let i = 0; i < cards.length; i++) {
+                const card = cards[i];
+                const div = document.createElement("div");
+                div.className = "skill-card h-[180px] w-full sm:w-[300px] p-5 border-2  rounded-lg flex flex-col gap-3 bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ";
+                div.innerHTML = `
+                    <h1 class="font-bold text-xl text-violet-500">${card.teach}</h1>
+                    <p>${card.learn}</p>
+                    <button class="w-[100px] py-1 font-bold bg-violet-500 rounded-lg text-white hover:bg-violet-600">View</button>
+                `;
+                container.appendChild(div);
+            }
+        }
