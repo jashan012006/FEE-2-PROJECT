45dcc480c1da3a8cc0d967f5fb4eccbb48b4a219
chiragkhosla <chiraguser1@gmail.com>
2025-10-24 21:14:31 +0530

    add name and skill level fields to skill cards and save to localStorage

--- Post_Skill.js
+++ Post_Skill.js
@@ -1,14 +1,20 @@
 function saveCard() {
+    const name= document.getElementById("nameInput").value.trim();
     const teach = document.getElementById("teachInput").value.trim();
+    const level= document.getElementById("levelSelect").value.trim();
     const learn = document.getElementById("learnInput").value.trim();
-
-    if (!teach || !learn) {
-        alert("Please fill both fields!");
+    if (!teach || !learn || !name || !level) {
+        alert("Please fill in all fields!");
         return;
     }
-
+    const card={
+        name: name,
+        teach: teach,
+        level: level,
+        learn: learn
+    }
     let cards = JSON.parse(localStorage.getItem("cards")) || [];
-    cards.push({ teach: teach, learn: learn });
+    cards.push(card);
     localStorage.setItem("cards", JSON.stringify(cards));
 
     alert("Card Posted Successfully!");
